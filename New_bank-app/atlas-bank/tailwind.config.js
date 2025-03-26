256ae94905427c66504ac85ba9fce0a1ecf95a79
Shivaprada Upadya <137288008+Shivaprada-upadya@users.noreply.github.com>
2025-03-26 16:35:02 +0530

    Delete New_bank-app directory

--- New_bank-app/atlas-bank/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-    content: ["./src/**/*.{html,js,ts,jsx,tsx}"],  // Adjust path as needed
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  };
-  
